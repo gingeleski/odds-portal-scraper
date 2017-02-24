b77705285c5aaf9b6d9506f1783701ab0026c4e9
Randy Gingeleski <rjg26247@gmail.com>
2017-02-24 09:14:45 -0500

    initial commit SoccerMatch

---
+++ SoccerMatch.py
@@ -0,0 +1,14 @@
+#
+# TODO TODO TODO
+#
+# Required fields:
+#
+#    start (datetime)
+#    end (datetime)
+#    team1
+#    team2
+#    team1_odds
+#    team2_odds
+#    draw_odds
+#    outcome (team1, team2, or draw)
+#
