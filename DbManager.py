556aa6bdc9c38c3cb9d78573d07c5007ea3e7052
Randy Gingeleski <rjg26247@gmail.com>
2017-02-25 16:34:14 -0500

    implement initial db file check and table creation

--- DbManager.py
+++ DbManager.py
@@ -1,11 +1,25 @@
+import os
 import sqlite3
+
+DB_FILENAME = 'oddsportal.db'
 
 class DatabaseManager():
     def __init__(self):
-        # TODO delete db file if it already exists
-        self.conn = sqlite3.connect('oddsportal.db')
+        try:
+            os.remove(DB_FILENAME)
+        except OSError:
+            pass
+        self.conn = sqlite3.connect(DB_FILENAME)
         self.cursor = self.conn.cursor()
-        # TODO create table and commit
+        self.cursor.execute('''DROP TABLE IF EXISTS matches''')
+        self.cursor.execute('''CREATE TABLE matches
+                                (area text, retrieved_from_url text,
+                                start_time text, end_time text, team1 text,
+                                team2 text, outcome text, team1_odds text,
+                                team2_odds text, draw_odds text)''')
+        self.conn.commit()
 
     def __del__(self):
         self.conn.close()
+
+my_db = DatabaseManager()
