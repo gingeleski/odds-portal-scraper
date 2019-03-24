b7163e2a651917eb87f25b2638463ad0291498e1
Randy Gingeleski <rjg26247@gmail.com>
2019-03-24 13:10:09 -0400

    #4 need to re-work js selectors...

--- predictions/scraper.py
+++ predictions/scraper.py
@@ -28,15 +28,25 @@ class Prediction():
         self.pick = -1
 
     def __repr__(self):
-        return 'TODO'
+        s = str()
+        s += 'Sport: ' + self.sport
+        s += 'Region: ' + self.region
+        s += 'League: ' + self.league
+        s += 'Start time: ' + self.start_time
+        s += 'Game name: ' + self.game_name
+        s += 'Game specifier: ' + self.game_specifier
+        s += 'URL: ' + self.url
+        s += 'Odds: ' + str(self.odds)
+        s += 'Pick: ' + str(self.pick)
+        return s
 
 
 async def main():
     # Set up headless browser and a page within it to work out of
     browser = await launch()
     page = await browser.newPage()
-    page.setUserAgent(USER_AGENT_STRING)
-    page.setViewport(VIEWPORT_DICT)
+    await page.setUserAgent(USER_AGENT_STRING)
+    await page.setViewport(VIEWPORT_DICT)
     # Read in Odds Portal username from environment variable ODDS_PORTAL_USERNAME
     try:
         username = os.environ['ODDS_PORTAL_USERNAME']
@@ -49,34 +59,46 @@ async def main():
         raise RuntimeError('Could not read environment variable ODDS_PORTAL_PASSWORD')
     # Navigate to Odds Portal login page
     await page.goto('https://www.oddsportal.com/login/')
+    # Inject script onto the page so we can leverage jQuery to get unique selectors later on
+    await page.evaluate('jQuery.fn.getPath=function(){for(var e,r=this;r.length;){var t=r[0],n=t.localName;if(!n)break;n=n.toLowerCase();var a=r.parent(),h=a.children(n);h.length>1&&(n+=":eq("+h.index(t)+")"),e=n+(e?">"+e:""),r=a}return e};')
     # Get a selector for the username field by running some JavaScript
-    username_selector = page.evaluate('TODO')
+    username_selector = await page.evaluate('if($(\'label:contains("sername")\').length<1){"ERROR"}else{var target_id=$(\'label:contains("sername")\').attr(\'for\');$(\'input#\'+target_id).getPath()}')
+    if username_selector == 'ERROR':
+        await page.screenshot({ 'path' : 'assumed_error.png' })
+        raise RuntimeError('Encountered issue trying to find username field at login form - see assumed_error.png !')    
     # Get username field into focus then type into it
-    page.click(username_selector)
-    page.keyboard.type(username)
+    await page.waitForSelector(username_selector, {'timeout':5000})
+    await page.click(username_selector)
+    await page.keyboard.type(username)
     # Get a selector for the password field by running some JavaScript
-    password_selector = page.evaluate('TODO')
+    password_selector = await page.evaluate('if($(\'label:contains("assword")\').length<1){"ERROR"}else{var target_id=$(\'label:contains("assword")\').attr(\'for\');$(\'input#\'+target_id).getPath()}')
+    if password_selector == 'ERROR':
+        await page.screenshot({ 'path' : 'assumed_error.png' })
+        raise RuntimeError('Encountered issue trying to find password field at login form - see assumed_error.png !')
     # Get password field into focus then type into it
-    page.click(password_selector)
-    page.keyboard.type(password)
+    await page.click(password_selector)
+    await page.keyboard.type(password)
     # Get a selector for the submit button by running some JavaScript
-    login_button_selector = page.evaluate('TODO')
+    login_button_selector = await page.evaluate('if($(\'button:contains("ogin")\').length<2){"ERROR"}else{$(\'button:contains("ogin"):eq(1)\').getPath()}')
+    if login_button_selector == 'ERROR':
+        await page.screenshot({ 'path' : 'assumed_error.png' })
+        raise RuntimeError('Encountered issue trying to find login button at login form - see assumed_error.png !')
     # Log in via that button then wait for redirect(s) to happen
     await asyncio.wait([
-        page.click(login_button_selector),
-        page.waitForNavigation(),
+        await page.click(login_button_selector),
+        await page.waitForNavigation(),
     ])
     # Use JavaScript to assert whether logout button is on page - otherwise assume error
-    is_there_logout_button = page.evaluate('true')
+    is_there_logout_button = await page.evaluate('true') # TODO
     if False == is_there_logout_button:
-        page.screenshot({ 'path' : 'assumed_error.png' })
+        await page.screenshot({ 'path' : 'assumed_error.png' })
         raise RuntimeError('Could not find logout button after login - see assumed_error.png !')
     # Get link to user profile
-    my_profile_link = page.evaluate('https://www.oddsportal.com/profile/gingeleski/')
+    my_profile_link = await page.evaluate('https://www.oddsportal.com/profile/gingeleski/')
     # Navigate to personal profile now
     await page.goto(my_profile_link)
     # Get list of users we're following via JavaScript
-    users_we_are_following = page.evaluate('["GGBet","OldTwinTowersFutbol"]')
+    users_we_are_following = await page.evaluate('["GGBet","OldTwinTowersFutbol"]') # TODO
     for user_we_are_following in users_we_are_following:
         link_to_users_predictions = 'https://www.oddsportal.com/profile/' + user_we_are_following + '/my-predictions/next/'
         this_output_folder = 'output/' + user_we_are_following
@@ -86,17 +108,17 @@ async def main():
         page_count = 1
         while True == is_there_another_page:
             # Use JavaScript to determine if there are any predictions on the page
-            are_there_predictions_on_page = page.evaluate('false')
+            are_there_predictions_on_page = page.evaluate('false') # TODO
             if True == are_there_predictions_on_page:
-                html_for_page = page.content()
+                html_for_page = await page.content()
                 # TODO scrape predictions off of page HTML
             # Save off image of this after checking if output directory exists
             if not os.path.exists(this_output_folder):
                 os.makedirs(this_output_folder)
             page_image_filename = user_we_are_following + '_' + page_count + '.png'
-            page.screenshot({ 'path' : this_output_folder + '/' + page_image_filename })
+            await page.screenshot({ 'path' : this_output_folder + '/' + page_image_filename })
             # Use JavaScript to determine if there's another page
-            is_there_another_page = page.evaluate('false')
+            is_there_another_page = await page.evaluate('false') # TODO
             page_count += 1
     await browser.close()
 
